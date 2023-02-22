ca20dbd54cb329ebd9e3fc63f65f7ddb060930c8
StuartMolnar <stuartmolnar@hotmail.com>
2023-02-22 03:22:25 -0800

    frontend

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-let url = 'https://usajobs-cors-proxy.westus3.cloudapp.azure.com:443/proxy/historicjoa?PageSize=10&PageNumber=2&PositionSeries=2210&StartPositionOpenDate=10-01-2015&EndPositionOpenDate=09-30-2016';
+let url = 'https://usajobs-cors-proxy.westus3.cloudapp.azure.com:443/proxy/historicjoa?PageSize=1000&PageNumber=2&PositionSeries=2210&StartPositionOpenDate=10-01-2015&EndPositionOpenDate=09-30-2016';
+
+console.log('accessing api...');
 
 fetch(url)
   .then(response => {
     return response.json()
    }).then(body => {
 
-    console.log(body);
-    console.log(data);
+    console.log('type:', typeof(body));
+    console.log('body:', body);
+    console.log('data:', body.data[0])
    }
 );
+
+//fix: edit the endpoint logic in the cors-proxy server to return the content in json already
